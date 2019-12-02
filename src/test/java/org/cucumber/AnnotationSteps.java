74489a1187e68e221f86f0956455514e5c0fc83e
Daria Kantsedal <dkantsedal@NX-DOMAIN.LAN>
2019-12-02 10:48:28 +0100

    240 automatisé

--- src/test/java/org/cucumber/AnnotationSteps.java
+++ src/test/java/org/cucumber/AnnotationSteps.java
@@ -1,32 +1,50 @@
 package org.cucumber;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import cucumber.api.java.fr.Alors;
 import cucumber.api.java.fr.Etantdonnéque;
 import cucumber.api.java.fr.Quand;
 
 public class AnnotationSteps {
+	static String login;
+	static String password;
+	WebDriver driver;
+	
 	@Etantdonnéque("je suis un utilisateur de Jpetstore")
 	public void je_suis_un_utilisateur_de_Jpetstore() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
-	}
-
-	@Etantdonnéque("je me connecte sur")
-	public void je_me_connecte_sur() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	   login = "j2ee";
+	   password = "j2ee";
 	}
 
 	@Quand("je me connecte")
 	public void je_me_connecte() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		System.setProperty("webdriver.gecko.driver", "src/test/resources/squash/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//navigation
+		driver.get("https://petstore.octoperf.com/");
+		driver.findElement(By.xpath("//a[.='Enter the Store']")).click();
+		driver.findElement(By.xpath("//a[.='Sign In']")).click();
+		driver.findElement(By.name("username")).clear();
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("username")).sendKeys(login);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("signon")).click();
+		
 	}
 
-	@Alors("je vois la page {string}")
-	public void je_vois_la_page(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	@Alors("je vois la page welcome")
+	public void je_vois_la_page() {
+		WebElement welcome = driver.findElement(By.id("WelcomeContent"));
+		assertEquals(welcome.getText(),"Welcome ABC!");
 	}
 	
 }
